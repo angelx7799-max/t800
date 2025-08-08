70a5a5923170e3dc7eaf6aa44b8b8d019d68b900
angelx7799-max <angelx7799@gmail.com>
2025-08-08 14:34:10 -0600

    Reemplazar playsound por mpg123

--- tts.py
+++ tts.py
@@ -1,10 +1,10 @@
 from gtts import gTTS
-from playsound import playsound
 import tempfile
+import os
 
 def say(text):
     print("T800:", text)
     tts = gTTS(text=text, lang='es')
     with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as fp:
         tts.save(fp.name)
-        playsound(fp.name)
+        os.system(f"mpg123 {fp.name}")
