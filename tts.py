2b3fc9af1affc0097f927afb4900e976d0b9c392
angelx7799-max <angelx7799@gmail.com>
2025-08-08 15:05:31 -0600

    nueva correccion

--- tts.py
+++ tts.py
@@ -1,10 +1,13 @@
-from gtts import gTTS
+import subprocess
 import tempfile
 import os
+from gtts import gTTS
 
 def say(text):
     print("T800:", text)
     tts = gTTS(text=text, lang='es')
-    with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as fp:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
         tts.save(fp.name)
-        os.system(f"mpg123 {fp.name}")
+        print(f"Reproduciendo: {fp.name}")
+        subprocess.run(["mpg123", fp.name])
+        os.remove(fp.name)
